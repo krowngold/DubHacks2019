0cb3b0e93260aaa29b9b8201a31d2920ddb7e089
Noah Krohngold <Noah.Krohngold@gmail.com>
2019-10-12 23:47:42 -0700

    Trying to fix google import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webapp2
 import jinja2
 import urllib
+import logging
 import os
 import sys
 import models
@@ -9,34 +10,57 @@ from google.appengine.ext import ndb
 from google.appengine.api import urlfetch
 reload(sys)
 sys.setdefaultencoding('utf8')
+sys.path.insert(1, '/Users/Noah Krohngold/google-cloud-sdk/platform/google_appengine')
+sys.path.insert(1, '/Users/Noah Krohngold/google-cloud-sdk/platform/google_appengine/lib/yaml/lib')
+sys.path.insert(1, 'lib')
+
+if 'google' in sys.modules:
+    del sys.modules['google']
 jinja_env = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+    )
 
 class User(ndb.Model):
   user_name = ndb.StringProperty(required=True)
   user_password = ndb.StringProperty(required=True)
   user_email = ndb.StringProperty(required=True)
 
+class DangerousLocation(ndb.Model):
+    location_name = ndb.StringProperty(required=True)
+    is_dark = ndb.BooleanProperty(required=True)
+    has_suspicious_people = ndb.BooleanProperty(required=True)
+    has_violence = ndb.BooleanProperty(required=True)
+    has_aggressive_drivers = ndb.BooleanProperty(required=True)
+
 class CreateProfile(webapp2.RequestHandler): #Allow users access
+    def checkUser(self, username, password):
+        user_list = User.query().fetch()
+        for user in user_list:
+            if (user.user_name == username and user.user_password == password):
+                return True
+        return False
+
+    def addUser(self, name, password, email):
+        User(user_name=self.request.get("username"), user_password=self.request.get("password"), user_email=self.request.get("email")).put()
+
     def get(self):
         template = jinja_env.get_template("templates/createprofile.html")
         self.response.write(template.render())
 
     def post(self):
-        user = User()
-        user.populate(
-                user_name = self.request.get("name"),
-                user_password = self.request.get("password"),
-                user_email = self.request.get("email")
-        )
-        user.put()
-        template_vars = {
-            "userName": self.request.get("name"),
-            "userPass": self.request.get("password"),
-            "userEmail": self.request.get("email"),
-        }
-        template = jinja_env.get_template("templates/main.html")
-        self.response.write(template.render(template_vars))
+        if (not self.checkUser(self.request.get("username"), self.request.get("password"))):
+            addUser(self.request.get("username"), self.request.get("password"), self.request.get("email"))
+            template_vars = {
+                "userName": self.request.get("username"),
+                "userPass": self.request.get("password"),
+                "userEmail": self.request.get("email"),
+            }
+            template = jinja_env.get_template("templates/main.html")
+            self.response.write(template.render(template_vars))
+        else:
+            template = jinja_env.get_template("templates/createprofile.html")
+            self.response.write(template.render())
+
 
 class LogIn(webapp2.RequestHandler):
     def checkUser(self, username, password):
@@ -51,20 +75,14 @@ class LogIn(webapp2.RequestHandler):
         self.response.write(template.render())
 
     def post(self):
-        if (self.checkUser(self.request.get("name"), self.request.get("password"))):
+        if (self.checkUser(self.request.get("username"), self.request.get("password"))):
             template = jinja_env.get_template("templates/main.html")
             self.response.write(template.render())
         else:
             template = jinja_env.get_template("templates/login.html")
             self.response.write(template.render())
 
-class MainPage(webapp2.RequestHandler): #show map and pins of each unsafe location and why it's unsafe
-    # def dataToJSON(self, data):
-    #     info = {}
-    #     print("placeholder")
-    #     with open("data.txt", "w") as outfile:
-    #         json.dump(info, outfile)
-
+class MainPage(webapp2.RequestHandler): 
     def get(self):
         template = jinja_env.get_template("templates/main.html")
         self.response.write(template.render())
@@ -76,8 +94,15 @@ class MainPage(webapp2.RequestHandler): #show map and pins of each unsafe locati
         template = jinja_env.get_template("templates/main.html")
         self.response.write(template.render(template_vars))
 
+class LocationReportPage(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template("templates/location.html")
+        self.response.write(template.render())
+
+
+
 app = webapp2.WSGIApplication([
-    ('/', LogIn),
+    ('/', CreateProfile),
     ('/create_profile', CreateProfile),
     ('/main', MainPage)
 ])
