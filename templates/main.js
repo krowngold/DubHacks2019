fca9f478b489eee3e1bea3837e08a500b23f6fb1
stellau <stellau@uw.edu>
2019-10-12 21:29:20 -0700

    fixed map

--- templates/main.js
+++ templates/main.js
@@ -1,4 +1,4 @@
-use strict;
+"use strict";
 
 (function() {
 
@@ -7,10 +7,48 @@ use strict;
   const qsa = helpers.qsa;
   const gen = helpers.gen;
 
-  window.addEventListener("load", init);
+  window.addEventListener("load", initMap);
 
-  function init() {
-    
+  // Note: This example requires that you consent to location sharing when
+  // prompted by your browser. If you see the error "The Geolocation service
+  // failed.", it means you probably did not give permission for the browser to
+  // locate you.
+  function initMap() {
+    let map, infoWindow;
+    map = new google.maps.Map(id('map'), {
+      // center: {lat: -34.397, lng: 150.644},
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 20
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Current Location');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
   }
 
 })();
